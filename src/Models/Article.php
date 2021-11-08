9dda1a83fc921016d3995e21568f61f8227e2b5b
Joaquin <joaquin.pinogiraldez@bbc.co.uk>
2021-11-08 18:54:57 +0000

    Implementation test

--- src/Models/Article.php
+++ src/Models/Article.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use PHPHtmlParser\Dom;
+use PHPHtmlParser\Options;
+
 class Article
 {
     private $title;
@@ -27,5 +30,45 @@ class Article
     public function getContent(): string
     {
         return $this->content;
+    }
+
+    public function getContentType(): array
+    {
+        $dom = new Dom;
+        $dom->setOptions(
+            // this is set as the global option level.
+            (new Options())
+                ->setStrict(false)
+                ->setPreserveLineBreaks(true)
+        );
+
+        $dom->loadStr($this->content);
+		$children = $dom->getChildren();
+
+        $output = [];
+
+        foreach ( $children as $child ) {
+            $tagName = $child->getTag()->name();
+
+            if ($tagName === "img") {
+                array_push(
+                    $output,
+                    [
+                        "type" => "img",
+                        "src"  => $child->getAttribute('src'),
+                        "alt"  => $child->getAttribute('alt')
+                    ]
+                );
+            } else {
+                array_push(
+                    $output,
+                    [
+                        "type"    => ($tagName === "p") ? "paragraph" : "text",
+                        "content" => $child->innerHtml
+                    ]
+                );
+            }
+        }
+        return $output;
     }
 }
