5d6dfcbdef44da1becd2a21060a5166c138f8e94
Joaquin <joaquin.pinogiraldez@bbc.co.uk>
2021-11-08 06:55:21 +0000

    Refactor

--- src/Models/File.php
+++ src/Models/File.php
@@ -11,14 +11,29 @@ class File
         $this->path = $fileName;
     }
 
+    private function getFile(): string
+    {
+        return realpath(__DIR__ . sprintf("/../../assets/%s", $this->path));
+    }
+
+    private function getFileContent(string $file): string
+    {
+        return file_get_contents($file);
+    }
+
+    private function getFileDecodeContent(string $file): array
+    {
+        return json_decode($file);
+    }
+
     public function getAllArticles(): array
     {
-        $file = realpath(__DIR__ . sprintf("/../../assets/%s", $this->path));
+        $file = $this->getFile();
         $contentDecode = [];
 
         if ($file) {
-            $content = file_get_contents($file);
-            $contentDecode = json_decode($content);   
+            $content = $this->getFileContent($file);
+            $contentDecode = $this->getFileDecodeContent($content);
         }
 
         return $contentDecode;
