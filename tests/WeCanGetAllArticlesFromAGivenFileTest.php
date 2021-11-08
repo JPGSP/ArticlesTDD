bfd3f9a0fac246bacdece30c28234387d2e3f92a
Joaquin <joaquin.pinogiraldez@bbc.co.uk>
2021-11-08 18:54:33 +0000

    Change test and add more

--- tests/WeCanGetAllArticlesFromAGivenFileTest.php
+++ tests/WeCanGetAllArticlesFromAGivenFileTest.php
@@ -47,12 +47,56 @@ class WeCanGetAllArticlesFromAGivenFileTest extends TestCase
         $this->assertEquals('Places to stay in Rome', $firstArticle->getTitle());
     }
 
-    public function test_we_have_an_image_in_the_third_article_of_the_file(): void
+    public function test_we_have_content_parsed_in_the_third_article_of_the_file(): void
     {
         $file = new File("articles.json");
         $articleCollection = $file->getAllArticles();
         $thirdArticle = $articleCollection[2];
 
-        $this->assertEquals('image', $thirdArticle->getContentType());
+        $expectedValue = [];
+        $expectedValue[0] = [
+            "type"    => "paragraph",
+            "content" => "<h1>New York</h1>"
+        ];
+        $expectedValue[1] = [
+            "type" => "img",
+            "src"  => "https://via.placeholder.com/1600x1200",
+            "alt"  => "New York"
+        ];
+
+
+        $this->assertEquals($expectedValue, $thirdArticle->getContentType());
+    }
+
+    public function test_we_have_content_parsed_in_the_second_article_of_the_file(): void
+    {
+        $file = new File("articles.json");
+        $articleCollection = $file->getAllArticles();
+        $thirdArticle = $articleCollection[1];
+
+        $expectedValue = [];
+        $expectedValue[0] = [
+            "type"    => "paragraph",
+            "content" => "<h1>Paris</h1><br />Here is a list of the best places to stay in Paris:"
+        ];
+        $expectedValue[1] = [
+            "type"    => "text",
+            "content" => "\n"
+        ];
+        $expectedValue[2] = [
+            "type" => "paragraph",
+            "content"  => "<strong>Hotel 1</strong>\nHotel 1 description"
+        ];
+        $expectedValue[3] = [
+            "type"    => "text",
+            "content" => "\n"
+        ];
+        $expectedValue[4] = [
+            "type" => "paragraph",
+            "content"  => "<strong>Hotel 2</strong>\nHotel 2 description"
+        ];
+
+
+        $this->assertEquals($expectedValue, $thirdArticle->getContentType());
     }
 }
