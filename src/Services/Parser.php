f1a76a35f9d97372466635a1cb92e03e206c15cd
Joaquin <joaquin.pinogiraldez@bbc.co.uk>
2021-11-14 23:30:34 +0000

    Change name Service to Services

---
+++ src/Services/Parser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services;
+
+use PHPHtmlParser\Dom;
+use PHPHtmlParser\Options;
+
+class Parser
+{
+    public static function parse(string $content): array
+    {
+        $dom = new Dom;
+        $dom->setOptions(
+            // this is set as the global option level.
+            (new Options())
+                ->setStrict(false)
+                ->setPreserveLineBreaks(true)
+        );
+
+        $dom->loadStr($content);
+		$children = $dom->getChildren();
+
+        $output = [];
+
+        foreach ($children as $child) {
+            $tagName = $child->getTag()->name();
+
+            if ($tagName === "img") {
+                array_push(
+                    $output,
+                    [
+                        "type" => "img",
+                        "src"  => $child->getAttribute('src'),
+                        "alt"  => $child->getAttribute('alt')
+                    ]
+                );
+            } else {
+                array_push(
+                    $output,
+                    [
+                        "type"    => ($tagName === "p") ? "paragraph" : "text",
+                        "content" => $child->innerHtml
+                    ]
+                );
+            }
+        }
+        return $output;
+    }
+}
